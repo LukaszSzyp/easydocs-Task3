f0088a2c0e70e7c877b8e0d1555917c3b6eea7d0
Lukasz Szypulewski <szyp86@gmail.com>
2022-07-02 21:30:25 +0200

    add remoteMethod postNewTask

--- todo/client/script.js
+++ todo/client/script.js
@@ -7,20 +7,16 @@ const submitNewTask = (e) => {
   let id = Number(Math.round(Math.random() * 10000000));
   const isDone = false;
   const title = inputTitle;
-  fetch("http://localhost:3000/api/todoLists", {
+  fetch("http://localhost:3000/api/todoLists/postNewTask", {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      id: id,
-      title: title,
-      isDone: isDone,
-    }),
+    body: JSON.stringify({ id: id, title: title, isDone: isDone }),
   })
     .then(() => {
-      alert("successful insert");
+      alert("successful insert ");
     })
     .catch((err) => {
       console.error(err);
--- todo/common/models/todo-list.js
+++ todo/common/models/todo-list.js
@@ -1,5 +1,34 @@
-'use strict';
+"use strict";
 
-module.exports = function(Todolist) {
+module.exports = function (Todolist) {
+  Todolist.postNewTask2 = function (body) {};
 
+  Todolist.postNewTask = function (id, title, isDone) {
+    Todolist.create({ id: id, title: title, isDone: isDone });
+  };
+
+  Todolist.remoteMethod("postNewTask", {
+    description: "add to toDo DB new task",
+    accepts: [
+      {
+        arg: "id",
+        type: "number",
+        require: true,
+      },
+      {
+        arg: "title",
+        type: "string",
+        require: true,
+      },
+      {
+        arg: "isDone",
+        type: "boolean",
+        require: true,
+      },
+    ],
+    http: {
+      path: "/postNewTask",
+      verb: "post",
+    },
+  });
 };
